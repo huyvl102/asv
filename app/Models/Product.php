ddec5cf03a335c230f0ba59dbec4fc8b67067707
HuyVL-FSU2 <huyvl@fsoft.com.vn>
2019-01-17 23:39:19 +0700

    Update database and model

---
+++ app/Models/Product.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $table = 'products';
+
+    public function category()
+    {
+        return $this->belongsTo('App\Models\Category', 'category_id', 'id');
+    }
+
+    public function image()
+    {
+        return $this->hasMany('App\Models\Image', 'product_id', 'id')->where('is_deleted', '0');
+    }
+}
