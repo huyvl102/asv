f7034f0a964382093a4df020c3d5682a8a5be086
HuyVL-FSU2 <huyvl@fsoft.com.vn>
2019-02-20 14:52:06 +0700

    Update code 20/2

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -8,9 +8,9 @@
         <li><a href="#contact-us" title="">Liên hệ</a></li>
     </ul>
     <div class="header-social">
-        <a href="https://business.facebook.com/Gia-c%C3%B4ng-c%C6%A1-kh%C3%AD-ch%C3%ADnh-x%C3%A1c-160759294493084"
+        <a href="https://www.facebook.com/Gia-c%C3%B4ng-c%C6%A1-kh%C3%AD-ch%C3%ADnh-x%C3%A1c-160759294493084"
            title="" class="fa fa-facebook"></a>
-        <a href="#" title="" class="fa fa-youtube-play"></a>
+        <a href="https://www.youtube.com/channel/UCe3Zl3Tj1y2d6KmNVFvJ-zw" title="" class="fa fa-youtube-play"></a>
         <a href="https://www.pinterest.com/nvoi/"
            title="" class="fa fa-pinterest"></a>
     </div>
